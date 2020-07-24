4b4c7e3f5a6f6bcfe1b3bc53204c24ca9e845556
OOCLDM\lisa8 <lisa8@oocl.com>
2020-07-24 15:41:44 +0800

    [TDD Parking Lot][Sam] test: should_return_ticket_num_when_parking_car_given_car

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class ParkingBoy {
+
+    public String parking (Car car) {
+        return "A001";
+    }
+}
--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,10 +1,23 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingBoy;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParkingBoyFacts {
     @Test
-    void write_your_first_test() {
+    void should_return_ticket_num_when_parking_car_given_car() {
+        //given
+        Car car = new Car();
 
+        //when
+        ParkingBoy parkingBoy = new ParkingBoy();
+        String result = parkingBoy.parking(car);
+
+        //then
+        assertEquals("A001", result);
     }
+
 }
