1da4f64579aa911be95361334ded02221d15bbc0
skymike <skymike11@qq.com>
2020-07-26 22:30:46 +0800

    [TDD Parking Lot][Sam] refactor : change data init

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -14,7 +14,6 @@ public class ParkingLot {
         cars = new HashMap<>();
         tickets = new ArrayList<>();
         historyTickets = new HashSet<>();
-        initParkingLotData();
     }
 
     public ParkingLot(Map<String, Car> cars, List<Ticket> tickets, Set<Ticket> historyTickets) {
@@ -45,31 +44,5 @@ public class ParkingLot {
 
     public void setTickets(List<Ticket> tickets) {
         this.tickets = tickets;
-    }
-
-    private void initParkingLotData() {
-        cars.put("T001", new Car("A001"));
-        cars.put("T002", new Car("A002"));
-        cars.put("T003", new Car("A003"));
-        cars.put("T004", new Car("A004"));
-        cars.put("T005", new Car("A005"));
-        cars.put("T006", new Car("A006"));
-        cars.put("T007", new Car("A007"));
-        cars.put("T008", new Car("A008"));
-        cars.put("T009", new Car("A009"));
-        cars.put("T0010", new Car("A010"));
-
-        tickets.add(new Ticket("A001", "T001"));
-        tickets.add(new Ticket("A002", "T002"));
-        tickets.add(new Ticket("A003", "T003"));
-        tickets.add(new Ticket("A004", "T004"));
-        tickets.add(new Ticket("A005", "T005"));
-        tickets.add(new Ticket("A006", "T006"));
-        tickets.add(new Ticket("A007", "T007"));
-        tickets.add(new Ticket("A008", "T008"));
-        tickets.add(new Ticket("A009", "T009"));
-        tickets.add(new Ticket("A010", "T0010"));
-
-        historyTickets.add(new Ticket("A100", "T100"));
     }
 }
--- src/main/java/com/oocl/cultivation/other/InitTool.java
+++ src/main/java/com/oocl/cultivation/other/InitTool.java
@@ -13,15 +13,15 @@ import java.util.*;
 public class InitTool {
     public static List<ParkingLot> initTestData() {
         List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot(initCarData(), initTicketData(), initHistoryData()));
         parkingLots.add(new ParkingLot(new HashMap<>(), new ArrayList<>(), new HashSet<>()));
         return parkingLots;
     }
 
     public static List<ParkingLot> init2FullParkinglotTestData() {
         List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(new ParkingLot());
-        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot(initCarData(), initTicketData(), initHistoryData()));
+        parkingLots.add(new ParkingLot(initCarData(), initTicketData(), initHistoryData()));
         return parkingLots;
     }
 
@@ -43,5 +43,44 @@ public class InitTool {
         parkingLots.add(new ParkingLot(cars, tickets, new HashSet<>()));
 
         return parkingLots;
+    }
+
+    private static Map<String, Car> initCarData() {
+        Map<String, Car> cars = new HashMap<>();
+
+        cars.put("T001", new Car("A001"));
+        cars.put("T002", new Car("A002"));
+        cars.put("T003", new Car("A003"));
+        cars.put("T004", new Car("A004"));
+        cars.put("T005", new Car("A005"));
+        cars.put("T006", new Car("A006"));
+        cars.put("T007", new Car("A007"));
+        cars.put("T008", new Car("A008"));
+        cars.put("T009", new Car("A009"));
+        cars.put("T0010", new Car("A010"));
+
+        return cars;
+    }
+
+    private static List<Ticket> initTicketData() {
+
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(new Ticket("A001", "T001"));
+        tickets.add(new Ticket("A002", "T002"));
+        tickets.add(new Ticket("A003", "T003"));
+        tickets.add(new Ticket("A004", "T004"));
+        tickets.add(new Ticket("A005", "T005"));
+        tickets.add(new Ticket("A006", "T006"));
+        tickets.add(new Ticket("A007", "T007"));
+        tickets.add(new Ticket("A008", "T008"));
+        tickets.add(new Ticket("A009", "T009"));
+        tickets.add(new Ticket("A010", "T0010"));
+        return tickets;
+    }
+
+    private static Set<Ticket> initHistoryData() {
+        Set<Ticket> historyTickets = new HashSet<>();
+        historyTickets.add(new Ticket("A100", "T100"));
+        return historyTickets;
     }
 }
